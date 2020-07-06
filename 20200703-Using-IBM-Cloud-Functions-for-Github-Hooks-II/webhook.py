a1a2db6548bda0cb8c61357ff18c27daddc7d2ec
Matt Hamilton <matthew.hamilton1@ibm.com>
2020-07-06 15:02:23 +0100

    Refactored to be easier to blog about

--- 20200703-Using-IBM-Cloud-Functions-for-Github-Hooks-II/webhook.py
+++ 20200703-Using-IBM-Cloud-Functions-for-Github-Hooks-II/webhook.py
@@ -3,72 +3,79 @@ import requests
 import xpring
 import sys
 
-def pay_xrp(reciever, mult=1):
-    amount = int(1 * 1e6 * mult) # drops - ideally related to "value" of contribution...
+def pay_xrp_testnet(wallet_seed, address, amount):
+    # Create a wallet instance using the seed
+    wallet = xpring.Wallet.from_seed(wallet_seed)
 
+    # The XRP testnet
     url = 'test.xrp.xpring.io:50051'
     client = xpring.Client.from_url(url)
 
-    txn = client.send(PAYMENTS['xrp']['wallet'], reciever, str(amount)) # create txn
-    res = client.submit(txn) # submit txn to the network
+    # Create a transaction
+    txn = client.send(wallet, address, str(amount))
 
-PAYMENTS = {
-    'xrp': {
-        'function': pay_xrp,
-        'wallet': None
-    },
-    'btc': {
-        'function': None,
-        'wallet': None
-    }
-}
+    # Submit txn to the network
+    res = client.submit(txn)
 
-def make_payment(payid, mult=1):
-    reciever = False
-    for address in payid['addresses']:
-        # Don't steal our imaginary imaginary money!
-        if address['paymentNetwork'] == 'XRPL' and address['environment'] == 'TESTNET':
-            reciever = address['addressDetails']['address']
-            break
-    if reciever:
-        return PAYMENTS['xrp']['function'](reciever, mult)
+    return res
 
-def pay_pusher(code_pusher, mult=1):
-    url = f'https://{code_pusher}.github.io/pay'
+
+def get_address_from_payid(payid, network, environment):
+    # Convert the PayID into a URL e.g.
+    # pay$username.github.io -> https://username.github.io/pay
+    local_part, domain = payid.split('$')
+    url = f'https://{domain}/{local_part}'
     response = requests.get(url)
     response.raise_for_status()
-    payid = response.json()
-    
-    make_payment(payid, mult)
 
-    result = f'I have paid {code_pusher}'
-    return result
+    data = response.json()
+
+    # Look for an address that matches the network
+    # and environment we want to use
+    for address in data['addresses']:
+        if address['paymentNetwork'] == network and \
+           address['environment'] == environment:
+            return address['addressDetails']['address']
+    
 
 def main(args):
     """
     Given a dictionary. Return a dictionary.
     """
-    payments = []
-    xrp_sender = args.get('xrp_sender', None)
-    if xrp_sender:
-        PAYMENTS['xrp']['wallet'] = xpring.Wallet.from_seed(xrp_sender)
-    # PAYMENTS['btc']['wallet'] = generate_bitcoin_wallet(args.get('btc_sender', None))
-    pusher = f'{args["pusher"]["name"]}'
-    url = f'https://{pusher}.github.io/pay'
-    response = requests.head(url)
-    # TODO: only pay on a merge to master (or pay on the PR, not the merge)
-    # TODO: secure xrp_sender
-    # TODO: deploy as function or action
-    if response.status_code < 400:
-        payments.append(pay_pusher(pusher, len(args['commits'])))
+
+    # The wallet seed (secret key) is passed in, as a bound
+    # parameter to this function
+    xrp_wallet_seed = args['xrp_wallet_seed']
+
+
+    # Extract the username of the pusher from the
+    # Github hook payload
+    pusher = args['pusher']['name']
+
+    # Assume a PayID on Github of this form
+    payid = f'pay${pusher}.github.io'
+
+    # Calculate the amount based on number of commits
+    # this is just an example and could be any metric
+    try:
+        num_commits = len(args['commits'])
+    except KeyError:
+        num_commits = 1
+    amount = 1000 * num_commits
+    
+    # Get the address from the PayID and make payment
+    address = get_address_from_payid(payid, 'XRPL', 'TESTNET')
+    res = pay_xrp_testnet(xrp_wallet_seed, address, amount)
+
     return {
-        'payments': payments
+        'address': address,
+        'amount': amount,
     }
 
 if __name__ == "__main__":
     # Invoke with python webhook.py hammertoe snzBUmvTTAzCCRwGvGfKeA6Zqn4Yf
     args = {
         'pusher': {'name': sys.argv[1]},
-        'xrp_sender': sys.argv[2]
+        'xrp_wallet_seed': sys.argv[2]
     }
     main(args)
